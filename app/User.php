58354cb22e2bd67d36298817c8495c20b2439918
David Forero <davidforero717@gmail.com>
2021-08-19 18:05:28 -0500

    se realiza relacion de tablas entre users y profiles por medio de las migraciones, en el modelo Users se realiza el metodo profile retornando el metodo hasOne de la class Profiler

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Symfony\Component\HttpKernel\Profiler\Profiler;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile(){
+
+        return $this->hasOne(Profiler::class);
+    }
 }
