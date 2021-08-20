8636397dfa877fa3b1383e0c2fc9e9b0217ad78b
David Forero <davidforero717@gmail.com>
2021-08-19 20:52:40 -0500

    se crea migracion de la tabla level, se agrega columna en tb users y se realiza la relacion con la tabla level, en los modelos se define las relaciones

--- app/User.php
+++ app/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
 
         return $this->hasOne(Profiler::class);
     }
+
+    public function level(){
+
+        return $this->belongsTo(Level::class);
+    }
 }
