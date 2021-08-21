7b66762dab0534961fb629dc8e8edb0abe69669b
David Forero <davidforero717@gmail.com>
2021-08-20 21:31:37 -0500

    se realiza creacion de tablas groups y group_user para realizar realicion de muchos a muchos con tb users

--- app/User.php
+++ app/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
 
         return $this->belongsTo(Level::class);
     }
+
+    public function groups(){
+
+        return $this->belongsToMany(Level::class)->withTimestamps();
+    }
 }
