ed38a49007b134b98351025847831405d58ae9c4
David Forero <davidforero717@gmail.com>
2021-09-09 14:15:15 -0500

    se realiza las consultas para el llamado de datos web, pais, nivel,y al grupo al que pertenece

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,29 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ $user->name }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <title>{{ $user->name }}</title>
 
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="row">
-                <div class="col-12">
-                    <h1> {{ $user->name }} </h1>
-                    <h3>{{ $user->email }} </h3>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-                    <p>
-                        <strong>Instragram</strong>: {{ $user->profile->instagram}}<br>      
-                        <strong>Github</strong>: {{ $user->profile->github}}<br>
-                        <strong>Web</strong>: {{ $user->profile->web}}<br>
-                    </p>
-                </div>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 my-3 pt-3 shadow">
+
+                <img src="{{ $user->image->url }}" class="float-left rounded-circle mr-2">
+                <h1> {{ $user->name }} </h1>
+                <h3>{{ $user->email }} </h3>
+
+                <p>
+                    <strong>Instragram</strong>: {{ $user->profile->instagram}}<br>
+                    <strong>Github</strong>: {{ $user->profile->github}}<br>
+                    <strong>Web</strong>: {{ $user->profile->web}}
+                </p>
+                <p>
+                    <strong>País</strong>: {{ $user->location->country}}<br>
+                    <strong>Nivel</strong>:@if ($user->level)
+                    <a href="#">{{ $user->level->name}}</a>
+                    @else
+                    ---
+                    @endif
+                </p>
+                <hr>
+                <p>
+                    <strong>Groups</strong>:
+                    @forelse($user->groups as $group)
+                    <span class="badge badge-primary">{{ $group->name }}</span>
+                    @empty
+                    <em>No pertenece a algún grupo</em>
+                    @endforelse
+                </p>
+                <hr>
+
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
         <style>
             html, body {
                 background-color: #fff;
